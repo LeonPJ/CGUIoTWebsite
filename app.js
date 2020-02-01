17906eaabe710f15e1bdeb5d91afe26af84701aa
LeonPJ <leon19941214@gmail.com>
2020-02-01 19:49:29 +0800

    creat form table

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
 
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     res.render('DeviceSignIn');
