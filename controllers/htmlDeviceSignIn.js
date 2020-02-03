d87711a9069366d737f0c263cc77bc380586abe8
LeonPJ <leon19941214@gmail.com>
2020-02-03 22:51:49 +0800

    HTTP request to platform

--- controllers/htmlDeviceSignIn.js
+++ controllers/htmlDeviceSignIn.js
@@ -1,3 +1,4 @@
+var request = require('request');
 var bodyParser = require('body-parser');
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -9,9 +10,30 @@ module.exports = function(app) {
 	});
 
     app.post('/SignIn', urlencodedParser, function(req, res){
-        res.send('Success Sign In');
+        var options = {
+            'method': 'POST',
+            'url': 'http://120.126.8.126/api/device?api_token=60kSeSf78sXjr0W9AtZoxMC9vIj1gzZP8GcdtHtTIaELm7PteocrYzBgjFVD',
+            'headers': {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            form: {
+              'name': req.body.DevieName,
+              'mac_address': req.body.DevieMAC,
+              'ip_address': req.body.DevieIP
+            }
+          };
+          request(options, function (error, response) { 
+            if (error) throw new Error(error);
+            console.log(JSON.parse(response.body).message);//將JSON字串解析成JavaScript物件
+            console.log(JSON.parse(response.body).data);
+            res.send(JSON.parse(response.body).message);
+
+          });
+        /*console.log(req.body.DevieIP);
+        console.log(req.body.DevieMAC);
         console.log(req.body.DevieName);
-        console.log(req.body.SelectTypeList);
+        console.log(req.body.SelectTypeList);*/
+                    
     });
 
 }
